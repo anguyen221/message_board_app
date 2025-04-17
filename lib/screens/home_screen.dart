983a939506f1941621de59f03a4e34267f90bac3
anguyen221 <anguyen221@student.gsu.edu>
2025-04-16 21:01:03 -0400

    Added hardcoded message boards to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,11 +3,32 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  final List<Map<String, dynamic>> messageBoards = const [
+    {'name': 'General Chat', 'icon': Icons.forum},
+    {'name': 'Tech Talk', 'icon': Icons.computer},
+    {'name': 'Study Zone', 'icon': Icons.school},
+    {'name': 'Random', 'icon': Icons.casino},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Message Boards')),
-      body: const Center(child: Text('Welcome to the Home Screen')),
+      appBar: AppBar(title: const Text('Message Boards')),
+      body: ListView.builder(
+        itemCount: messageBoards.length,
+        itemBuilder: (context, index) {
+          final board = messageBoards[index];
+          return ListTile(
+            leading: Icon(board['icon'], color: Colors.blue),
+            title: Text(board['name']),
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Open "${board['name']}" board')),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
