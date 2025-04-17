c445164f5e49118e3f461fe220c3825d3cd95628
anguyen221 <anguyen221@student.gsu.edu>
2025-04-16 20:37:44 -0400

    Created a proper splash screen and separated it from main.dart

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.forum, size: 100, color: Colors.white),
+            SizedBox(height: 20),
+            Text(
+              'Message Board App',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
